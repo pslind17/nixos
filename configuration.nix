647e76a81d92ee9163da88d48e5d5667844c1fd1
p1diddy <125328571+p1diddy@users.noreply.github.com>
2024-03-30 14:36:32 -0700

    added flake support

--- configuration.nix
+++ configuration.nix
@@ -114,6 +114,8 @@
     setSocketVariable = true;
   };
 
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
